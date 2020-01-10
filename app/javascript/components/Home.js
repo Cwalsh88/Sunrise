edf76aa09f04e2b1bb043b77382cadc2eefe16dc
Cwalsh88 <Chriswalsh003@gmail.com>
2020-01-09 20:23:45 -0600

    First Completed Chart

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -7,17 +7,73 @@ class Home extends React.Component {
     return (
       <React.Fragment>
         <div className="container">
-          <div className="row projects"> 
-            Projects
+          <div className="row project">
+            <div className="item">
+              Earth Day Strike
+            </div>
+            <div className="item">
+              South Side Hub
+            </div>
+            <div className="item">
+              Endorsements
+            </div>
+            <div className="item">
+              National Legislation
+            </div>
+            <div className="item">
+              College Outreach
+            </div>
+            <div className="item">
+              Fellowship
+            </div>
           </div>
-          <div className="row teams">
-            Teams
+          <div className="row team">
+            <div className="item">
+              Actionss
+            </div>
+            <div className="item">
+              Art
+            </div>
+            <div className="item">
+              Comms
+            </div>
+            <div className="item">
+              Fundraising
+            </div>
+            <div className="item">
+              Onboarding
+            </div>
+            <div className="item">
+              Presentation
+            </div>
+            <div className="item">
+              Strategic Partnership
+            </div>
+            <div className="item">
+              Tech
+            </div>
           </div>
           <div className="row leadership">
-            Leadership
+            <div className="item">
+              Emailer
+            </div>
+            <div className="item">
+              Press Handler
+            </div>
+            <div className="item">
+              Tabler
+            </div>
+            <div className="item">
+              Logistor
+            </div>
+            <div className="item">
+              Interpersonal
+            </div>
           </div>
           <div className="row hc">
-            Hub Coordinator
+            <div className="item">
+              Hub Coordinator
+            </div>
           </div>
         </div>  
       </React.Fragment>
